587ad62fe9956ad13234ebd4851c6c1fc8ea1e51
log :3 <65839350+logzinga@users.noreply.github.com>
2022-10-12 11:15:59 +1100

    Internet connection test

--- setup.sh
+++ setup.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+echo "Are you connected to the internet? [ y / n ]"
+read INTERNETCONNECTION
+
+if [ $INTERNETCONNECTION = n ]
+    then
+        echo "You must be connected to the internet to continue"
+        sleep 2
+        clear 
+        exit
+fi
