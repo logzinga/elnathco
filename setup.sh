74e515f47c7d04160a3d8a2167cc632b3c2c97bf
log :3 <65839350+logzinga@users.noreply.github.com>
2022-10-12 11:42:52 +1100

    creates drives

--- setup.sh
+++ setup.sh
@@ -10,3 +10,27 @@ if [ $INTERNETCONNECTION = n ]
         clear 
         exit
 fi
+
+clear
+
+timedatectl set-ntp true
+ntpd -qg
+hwclock -w
+
+clear
+
+parted /dev/sda mklabel gpt
+parted /dev/sda mkpart "EFI" fat32 1MiB 301MiB
+parted /dev/sda set 1 esp on
+
+parted /dev/sda mkpart "swap" linux-swap 301MiB 8GiB
+
+parted /dev/sda mkpart "root" ext4 8GiB 100%
+
+mkfs.ext4 /dev/sda3
+mkswap /dev/sda2
+mkfs.fat -F 32 /dev/sda1
+
+mount /dev/sda3 /mnt 
+mount --mkdir /dev/sda1 /mnt/boot
+swapon /dev/sda2
