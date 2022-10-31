ce6960a511164e16caf4ba08e1943c38f7f7626d
logzinga <65839350+logzinga@users.noreply.github.com>
2022-10-31 19:41:15 +1100

    removed reinstall of git

--- isoprofile/airootfs/root/Desktop/install.sh
+++ isoprofile/airootfs/root/Desktop/install.sh
@@ -3,7 +3,6 @@
 echo "This script will update the installer with the latest version avaliable on GitHub."
 pause 2
 cd /
-pacman -Sy git
 git clone https://github.com/logzinga/elnathco
 cd elnathco
 ./setup.sh 
